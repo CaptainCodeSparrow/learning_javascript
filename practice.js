a50c0f75a331c34e77488f5493133df34ff570d1
captaincodesparrow <captaincodesparrow@gmail.com>
2024-10-31 17:28:20 +0600

    some practices

---
+++ practice.js
@@ -0,0 +1,28 @@
+const x = 'CaptainCode';
+const y = 43;
+// const z = true;
+// const q = 4.5;
+// const t = null;
+// const u = undefined;
+
+// console.log(typeof u)
+
+// const nothing = `Hello ${x} you have got ${y} outof 50`;
+// console.log(nothing) ;
+// const r = 'Captain, Code, Sparrow';
+// console.log(r.split(', '));
+
+
+// Array
+
+// const names = ['masud', 'nirzon', 'turzo'];
+// names.push('sadi')
+// console.log(names)
+
+
+// for loop
+
+// for(let name = 0; name <= 9; name++){
+//     console.log('Allah')
+
+// }
